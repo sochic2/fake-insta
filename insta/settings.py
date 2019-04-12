6079ba4d4cd2fb945c947a12bc4ee5a13efe9dae
sochic2 <netzzang12@gmail.com>
2019-04-12 02:37:57 +0000

    02_CRUD & Image Upload

--- insta/settings.py
+++ insta/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'insta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'insta', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from . import views
 
 app_name = 'posts'
 
 urlpatterns = [
-    
+    path('<int:post_pk>/delete', views.delete, name='delete'),
+    path('<int:post_pk>/edit', views.update, name='update'),
+    path('create', views.create, name='create'),
+    path('', views.list, name='list'),
 ]
