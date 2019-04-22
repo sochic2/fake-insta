ad8d16bbf5f26c36f1f0852429bb0fb3ac1c4cbe
sochic2 <netzzang12@gmail.com>
2019-04-22 08:37:12 +0000

    11_make social login

--- insta/settings.py
+++ insta/settings.py
@@ -28,9 +28,25 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+
+AUTHENTICATION_BACKENDS = (
+ 
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    # 'allauth.account.auth_backends.AuthenticationBackend',
+
+)
+
+
 # Application definition
 
 INSTALLED_APPS = [
+       
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'imagekit',
     'bootstrap4',
     'django_extensions',
@@ -42,7 +58,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth.socialaccount.providers.kakao',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,4 +146,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'accounts.User'  # 기본 user 모델을 오버라이드 하는 과정..
-
+LOGIN_REDIRECT_URL = 'posts:list'
